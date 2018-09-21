e54ce6b25a491d95ffd56625468507895c53c21a
linc <376817512@qq.com>
2018-09-21 17:28:54 +0800

    分开短音符长音符的参数

---
+++ NoteFeatureProcessor.py
@@ -0,0 +1,54 @@
+
+import os
+import pickle
+import myprocesser
+
+class BasicNoteFeatureProcessor():
+    def __init__(self):
+        self.posfix = '_feature'
+
+    def makeFeatureFilePath(self, filePath):
+        arr = os.path.splitext(filePath)
+        if arr[1] == '.raw':
+            return filePath
+
+        return arr[0] + self.posfix + '.raw'
+
+    def extract(self, audioFilePath):
+        print('BasicNoteFeatureProcessor extract nothing')
+        return []
+
+    def save(self, filePath, features, labels):
+        targetFilePath = self.makeFeatureFilePath(filePath)
+        with open(targetFilePath, 'wb') as file:
+            pickle.dump(features, file)
+            pickle.dump(labels, file)
+            print('raw file saved', features.shape)
+
+    def load(self, filePath):
+        targetFilePath = self.makeFeatureFilePath(filePath)
+        if not os.path.exists(targetFilePath):
+            print('filePath not exist', targetFilePath)
+            return [], []
+
+        with open(targetFilePath, 'rb') as file:
+            features = pickle.load(file)
+            labels = pickle.load(file)
+
+        return features, labels
+
+class ShortNoteFeatureProcessor(BasicNoteFeatureProcessor):
+    def __init__(self):
+        self.posfix = '_feature_short'
+
+    def extract(self, audioFilePath):
+        print('ShortNoteFeatureProcessor extract')
+        return myprocesser.LoadAndProcessAudio(audioFilePath)
+
+class LongNoteFeatureProcessor(BasicNoteFeatureProcessor):
+    def __init__(self):
+        self.posfix = '_feature_long'
+
+    def extract(self, audioFilePath):
+        print('LongNoteFeatureProcessor extract')
+        return myprocesser.LoadAndProcessAudio(audioFilePath)
