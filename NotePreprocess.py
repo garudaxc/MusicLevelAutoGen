1365d5409b5da70419e94a204db7ff2f46d78e37
linc <376817512@qq.com>
2018-09-26 11:23:16 +0800

    从QQDancerFeature里分出来，待删掉一些代码

---
+++ NotePreprocess.py
@@ -0,0 +1,187 @@
+import madmom
+from madmom.features.downbeats import *
+import librosa
+import numpy as np
+import time
+import scipy
+import os.path
+
+def MSL(beats):
+    # 最小二乘直线拟合
+    numBeats = len(beats)
+
+    AT = np.matrix([np.ones(numBeats), range(numBeats)])
+    b = np.matrix(beats)
+    x = (AT * AT.T).I * AT * b.T
+    #logger.info(x)
+
+    a = x[1, 0]
+    b = x[0, 0]
+
+    beat_times2 = np.arange(numBeats)
+    beat_times2 = beat_times2 * a
+
+    return a, b
+
+def beat_intervals(samples):
+    # 计算间隔
+    numSamples = len(samples)
+    t = np.zeros(numSamples-1)
+    for i in range(numSamples-1):
+        t[i] = samples[i+1] - samples[i]
+        
+    mean = sum(t) / len(t)
+    return t
+
+
+def min_measure(mean, dist):
+    index = 0
+    measure = np.inf
+    for i in range(len(mean)):
+        m = mean[i] * mean[i] + dist[i]
+        if m < measure:
+            measure = m
+            index = i
+    return measure, index
+
+
+def diff(beats, a):
+    winSize = 32
+    numBeat = len(beats) - winSize
+    mean = np.zeros(numBeat)
+    dist = np.zeros(numBeat)
+    for i in range(numBeat):
+        v = beats[i:i+winSize]
+        mean[i] = sum(v) / float(winSize) - a
+        v = v - a
+        dist[i] = sum(v * v)
+
+    cnt = int(numBeat / 3)
+    
+    m, i1 = min_measure(mean[:cnt], dist[:cnt])
+    #logger.info(m, ' ', i1)    
+    m, i2 = min_measure(mean[-cnt:], dist[-cnt:])
+    
+    i2 = numBeat - cnt + i2
+    return i1, i2
+        
+        
+def calc_beat_interval(beats, i1, i2):
+    size = 32
+    bpm1 = (beats[i1 + size] - beats[i1]) / float(size)
+    bpm2 = (beats[i2 + size] - beats[i2]) / float(size)
+    bpm = (bpm1 + bpm2) / 2.0
+    numbeat = (beats[i2] - beats[i1]) / bpm
+    
+    a2 = (beats[i2] - beats[i1]) / round(numbeat)    
+    b1 = beats[i1] - a2 * i1
+    b2 = beats[i2] - a2 * i2
+    
+    return a2, b1
+
+
+def CalcBarInterval(beat_times):
+    numBeats = len(beat_times)
+    itvals = beat_intervals(beat_times)
+    #logger.info('mean ' + str(mean))
+
+    #最小二乘计算固定间隔拍子·
+    a, b = MSL(beat_times)  
+
+    # new_beat_times = np.arange(numBeats) * a + b
+    return a, b
+
+
+def NormalizeInterval(beat, threhold = 0.02, abThrehold=0.333):
+    # 截掉前后不太准的beat
+    interval = beat[1:,0] - beat[:-1, 0]
+
+    aver = np.average(interval)
+    diff = np.abs(interval - aver)
+
+    abnormal = np.nonzero(diff > threhold)[0]
+    logger.info('abnormal count', abnormal.size, abnormal)
+
+    if abnormal.size > beat.shape[0] * abThrehold:
+        # 拍子均匀程度太低，自动生成失败
+        return -1, abnormal.size / beat.shape[0]
+
+    # #删除头尾不均匀的拍子
+    # #从前往后删
+    # count0 = 0
+    # for i in range(len(abnormal)):
+    #     if i == abnormal[i]:
+    #         count0 = count0 + 1
+
+    # #从后往前删
+    # count1 = 0
+    # for i in range(-1, -1-len(abnormal), -1):
+    #     if len(interval) + i == abnormal[i]:
+    #         count1 = count1 + 1
+    # logger.info('del %d items form begining %d items form end' % (count0, count1))
+
+    return 0, len(beat)
+
+# 新版本移除了这个函数，暂时替换一下，考虑用新版本的函数
+def madmom_features_downbeats_filter_downbeats(beats):
+    """
+
+    Parameters
+    ----------
+    beats : numpy array, shape (num_beats, 2)
+        Array with beats and their position inside the bar as the second
+        column.
+
+    Returns
+    -------
+    downbeats : numpy array
+        Array with downbeat times.
+
+    """
+    # return only downbeats (timestamps)
+    return beats[beats[:, 1] == 1][:, 0]
+    
+def CalcBpmET(y, sr, duration, **args):
+    # calc downbeat entertime
+    analysisLength = 60
+    minimumMusicLength = 110
+    maximumMusicLength = 360
+    numThread = 1
+    threhold = 0.02
+    abThrehold = 0.333
+
+    start = int(duration * 0.3)
+    if start + analysisLength > duration:
+        start = duration - analysisLength
+        start = max(start, 0)
+        analysisLength = min(analysisLength, duration)
+        
+    clipTime = np.array([start, start+analysisLength])      
+    # logger.info('duration', duration, 'start', start)
+    clip = librosa.time_to_samples(clipTime, sr=sr)
+    # logger.info('total', y.shape, 'clip', clip)
+    yy = y[clip[0]:clip[1]]
+
+    processer = RNNDownBeatProcessor(num_threads=numThread)
+    downbeatTracking = DBNDownBeatTrackingProcessor(beats_per_bar=4, transition_lambda=1000, fps=100)
+    beatProba = processer(yy)
+
+    beatIndex = downbeatTracking(beatProba)
+    
+    firstBeat, lastBeat = NormalizeInterval(beatIndex, threhold=threhold, abThrehold=abThrehold)
+    if firstBeat == -1:        
+        logger.error(PROBABLY_NOT_MUSIC, 'generate error,numbeat %d, abnormal rate %f' % (len(beatIndex), lastBeat))
+        return 0, 0
+
+    newBeat = beatIndex[firstBeat:lastBeat]
+    downbeat = madmom_features_downbeats_filter_downbeats(newBeat)
+    downbeat = downbeat + librosa.samples_to_time(clip, sr = sr)[0]
+
+    barInter, etAuto = CalcBarInterval(downbeat)
+    
+    # enter time is less than a bar interval
+    etAuto = etAuto % barInter
+
+    bpm = 240.0 / barInter
+
+    return bpm, etAuto
