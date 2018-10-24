779a1140e9aa8d0b8cc52004375f2af5b1804e67
linc <376817512@qq.com>
2018-10-24 14:52:50 +0800

    transcode param

--- UnitTest.py
+++ UnitTest.py
@@ -404,7 +404,7 @@ def RunNoteLevelGenerator():
         audioFilePath = TFLstm.MakeMp3Pathname(song)
         levelFilePath = os.path.join(os.path.dirname(audioFilePath), song+'.xml')
         runStart = time.time()
-        generator.run(audioFilePath, levelFilePath, outputDebugInfo=True)
+        generator.run(audioFilePath, levelFilePath, isTranscodeByQAAC=True, outputDebugInfo=True)
         print('cost _______ ', song, time.time() - runStart)
 
     generator.releaseResource()
